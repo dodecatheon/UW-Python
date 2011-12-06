07d0727487dd54df3cb910cba8904414d6a2df04
Ted Stern <dodecatheon@gmail.com>
2011-12-05 20:06:11 -0800

    add wraps decorator for generator decorator

--- fall2011/week08/gendec.py
+++ fall2011/week08/gendec.py
@@ -3,7 +3,9 @@
 Due Week09, 2011/12/05:
 Create a decorator that turns a function into a generator
 """
+from functools import wraps
 def generator(f):
+    @wraps(f)
     def gendec(t):
         for x in t:
             yield f(x)
@@ -12,17 +14,23 @@ def generator(f):
 if __name__ == "__main__":
     @generator
     def odd(i):
+        "Input i.  Returns 0 if i even, 1 if i odd."
         return i % 2
     
     for y in odd(xrange(1,4)): print y
     
+    print ""
+    print "odd.__name__ =", odd.__name__
+    print "odd.__doc__ =", odd.__doc__
 """
-Results of run:
-------------------------------------------------------------------------
-ted@baobob:0 22:33:02 ~/uwpython/fall2011/week08
-$ ./gendec.py 
+ted@baobob:0 20:01:44 ~/uwpython/fall2011/week08
+$ ./gendec.py
 1
 0
 1
-ted@baobob:0 22:33:06 ~/uwpython/fall2011/week08
+
+odd.__name__ = odd
+odd.__doc__ = Input i.  Returns 0 if i even, 1 if i odd.
+ted@baobob:0 20:01:46 ~/uwpython/fall2011/week08
+$ 
 """
