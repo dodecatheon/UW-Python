910e4c508d48179aa103cc03c991714b34993da8
Ted Stern <dodecatheon@gmail.com>
2012-02-14 20:57:36 -0800

    WSGI test, with logging of unhandled queries

--- winter2012/week06/echo_wsgi.py
+++ winter2012/week06/echo_wsgi.py
@@ -29,7 +29,7 @@ message_template = """
 <title>Echo response</title>
 </head>
 <body>
-Message: %s
+%s
 </form>
 </body>
 </html>
@@ -47,19 +47,45 @@ notfound_template = """
 </html>
 """
 
+messages = []
+unhandled_queries = []
+
 # must be named 'application' to work with our wsgi simple server
-def application(environ, start_response): 
+def application(environ, start_response):
+    global messages
+    global unhandled_queries
     status = '200 OK'
     response_headers = [('Content_type', 'text/HTML')]
     start_response(status, response_headers)
     # send different page depending on URL path
-    path = environ['PATH_INFO'] 
+    path = environ['PATH_INFO']
     if path == '/echo_wsgi.html':
         page = form_page
     elif path == '/echo_wsgi.py':
-        # get message from URL query string, parse_qs returns a list for each key
-        page = message_template % (
-            urlparse.parse_qs(environ['QUERY_STRING'])['message'][0])
+
+        # Generalize the query so that we can do a number of different options
+        query_string = urlparse.parse_qs(environ['QUERY_STRING'])
+        print query_string
+
+        # Brian asked me to do some extra credit, to handle other
+        # sorts of queries:
+        if query_string.has_key('message'):
+            # This is what you asked for in the assignment
+            messages.append('Message %d: %s' % (
+                len(messages)+1,
+                query_string['message'][0]))
+            page = message_template % '<br>\n'.join(messages)
+        else:
+            # This is a logging honeypot ... it shows what queries have
+            # been tried, the entire time the server has been up.
+            i = len(unhandled_queries)
+            for query, arglist in query_string.iteritems():
+                i +=1
+                unhandled_queries.append(
+                    'Query number %d, "%s", with argument "%s", not handled yet' % \
+                    (i, query, arglist[0] ))
+            page = message_template % '<br>\n'.join(unhandled_queries)
+
     else:
         page = notfound_template % path
     return [ page ] # list of strings - must return iterable, not just a string
